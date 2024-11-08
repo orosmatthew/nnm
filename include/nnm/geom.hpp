9617f4cd9444086fbb2476e7a89e6c2f2613d380
orosmatthew <orosmatthew@pm.me>
2024-11-08 11:03:51 -0500

    Refactor Arc2 from_points

--- include/nnm/geom.hpp
+++ include/nnm/geom.hpp
@@ -1026,9 +1026,12 @@ public:
         const Real angle_from = pivot->angle_to(from);
         const Real angle_to = pivot->angle_to(to);
         const Real angle_through = pivot->angle_to(through);
-        const Real angle = angle_in_range(angle_through, angle_from, angle_to) ? angle_to - angle_from
-            : angle_to < angle_from ? static_cast<Real>(2) * pi() - abs(nnm::normalize_angle(angle_to - angle_from))
-                                    : -static_cast<Real>(2) * pi() + abs(nnm::normalize_angle(angle_to - angle_from));
+        const Real two_pi = static_cast<Real>(2) * pi<Real>();
+        const Real angle_diff = abs(nnm::normalize_angle(angle_to - angle_from));
+        const bool in_range = angle_in_range(angle_through, angle_from, angle_to);
+        const Real angle = in_range ? angle_to - angle_from
+            : angle_to < angle_from ? two_pi - angle_diff
+                                    : -two_pi + angle_diff;
         return Arc2 { pivot.value(), from, angle };
     }
 
